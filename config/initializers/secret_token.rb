ce54c2f073f94702ff019a006b77c5210f067e63
nakamura <nakamura@www.pinejuice>
2010-08-28 16:48:17 +0900

    lifepack_task_racoard new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifepackTaskRecord::Application.config.secret_token = '5edd9b6d3b48ece94b907bdeb8df26d9b3b9a2a94ecba6b6a604b54f7de655c2385e36362998d4effa0f55cacb9bf60d1fa059fd10a4b630cb63e19aaefb49a8'
